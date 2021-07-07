0f56b699bd02bece474385227ddb191c121b0381
Kajal Damji Gada <16885692+KajalGada@users.noreply.github.com>
2021-07-07 12:16:54 -0700

    Create leetcode 1237 find positive integer solution for a given equation sol2.py

---
+++ 1237_find_positive_integer_solution_for_a_given_equation/leetcode 1237 find positive integer solution for a given equation sol2.py
@@ -0,0 +1,33 @@
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        
+        res = []
+        x = 1
+        y = z
+        
+        while (x <= z) and (y >= 1):
+            
+            sol = customfunction.f(x, y)
+            if sol is z:
+                res.append([x, y])
+                x += 1
+                y -= 1
+                
+            elif sol > z:
+                y -= 1
+                
+            else:
+                x += 1
+        
+        return res
